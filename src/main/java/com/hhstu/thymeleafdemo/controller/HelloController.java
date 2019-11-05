f06b47a5a400a2c8e637007a27fd0bbe515b7e5c
Leefs <2556704509@qq.com>
2019-11-05 22:03:17 +0800

    Thymeleaf练习

--- src/main/java/com/hhstu/thymeleafdemo/controller/HelloController.java
+++ src/main/java/com/hhstu/thymeleafdemo/controller/HelloController.java
@@ -1,8 +1,12 @@
 package com.hhstu.thymeleafdemo.controller;
 
+import com.hhstu.thymeleafdemo.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
@@ -11,6 +15,26 @@ public class HelloController {
     public String index(Model model){
         model.addAttribute("msg","SpringBoot集成Thymeleaf");
         //return 就是你页面的路径名称,默认路径在templates路径下（不需要带.html后缀）
+        User user = new User();
+        user.setId(1);
+        user.setNick("李林超博客");
+        user.setPhone("123445566");
+        user.setEmail("996@163.com");
+        user.setAddress("北京朝阳区");
+        model.addAttribute("user",user);
+
+        List<User> userList = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            User user1 = new User();
+            user1.setId(i);
+            user1.setNick("李林超博客"+i);
+            user1.setPhone("12344556"+i);
+            user1.setEmail(i+"996@163.com");
+            user1.setAddress("北京朝阳区"+i);
+            userList.add(user1);
+        }
+        model.addAttribute("userList",userList);
+
         return "index";
     }
 }
